d3d03987fb820dc0157cb49d6f9a93ebe18e0067
MacNikSipeykin <niksipeykin@gmail.com>
2025-07-09 18:33:06 +0300

    Add BAN_HUMMER

--- src/main/java/com/app/bysell/configurations/SecurityConfig.java
+++ src/main/java/com/app/bysell/configurations/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,6 +16,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
   private final CustomUserDetailsService userDetailsService;
--- src/main/java/com/app/bysell/models/User.java
+++ src/main/java/com/app/bysell/models/User.java
@@ -63,6 +63,10 @@ public class User implements UserDetails {
     return roles;
   }
 
+  public boolean isAdmin(){
+    return roles.contains(Role.ROLE_ADMIN);
+  }
+
   @Override
   public String getUsername() {
     return email;
